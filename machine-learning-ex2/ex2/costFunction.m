73283b47cc883380491daa06ea8c33d1d721142c
xdliu002 <7lxd@tongji.edu.cn>
2017-03-17 23:55:54 +0800

    Task 1 Done.
    Have some problems with Task2.

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -7,7 +7,7 @@ function [J, grad] = costFunction(theta, X, y)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -20,12 +20,13 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+J =  ((-y) * log(sigmoid(theta'* X' )) -(1 - y)*log(1- sigmoid(theta' * X'))) / m;
 
+%J = sum(J,:);
 
-
-
-
-
+% for j = 1 : size(theta)
+%   grad(j) = (sigmoid(theta'* X') - y) * X(j) / m;
+% end
 
 % =============================================================
 
